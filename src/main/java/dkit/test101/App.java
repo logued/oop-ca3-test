86177765067429096d602c80b3d54182d9d4e047
Dermot <dermot.logue@dkit.ie>
2020-12-12 09:45:00 +0000

    Initial commit

---
+++ src/main/java/dkit/test101/App.java
@@ -0,0 +1,13 @@
+package dkit.test101;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
